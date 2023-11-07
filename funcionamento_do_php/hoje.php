8a1ba8a2cc56c1a1291a15a8f707bec0a5900689
Adijair <adijairp05@gmail.com>
2023-11-07 16:17:46 -0300

    exenpllo do funcionamento do php

---
+++ funcionamento_do_php/hoje.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dia <?php echo date('d/m/Y'); ?></title>
+</head>
+<body>
+    <h1>
+        estamos em <?php echo date('Y'); ?> e hoje é dia
+        <? echo date('d/m'); ?>
+    </h1>
+    <p>
+        Esta página foi gerenciada ás <?php echo date('H'); ?>
+        horas e <?php echo date('i'); ?> minutos.
+    </p>
+</body>
+</html>
